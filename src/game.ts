802339b29d7fcf37cb9862597088d3fe98442d07
yoann <yo252yo@gmail.com>
2021-04-20 14:33:31 +0200

    growth mechanic

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,79 @@
 
 import * as SocketManager from './modules/server_socket_manager.js';
 
-var broadcast = function(){
+var SIZE = 200;
+var ROOT = [SIZE/2, SIZE-1];
+var FOREST = [[ROOT]];
+var HOSPITABILITY = 0.9;
+
+
+var emptyBoard = function(){
   var board: boolean[][] = [];
 
-  var size = 600;
-
-  for(var i=0; i< size; i++){
+  for(var i=0; i< SIZE; i++){
     board[i] = [];
-    for(var j=0; j< size; j++){
-      board[i][j] = (Math.random() > 0.5);
+    for(var j=0; j< SIZE; j++){
+      board[i][j] = false;
+    }
+  }
+  return board;
+}
+
+var broadcast = function(){
+  var board = emptyBoard();
+  for(var chain of FOREST) {
+    for(var node of chain) {
+      board[node[0]][node[1]] = true;
     }
   }
 
-  var hospitability = Math.random();
-
-  var o = {board: board, hospitability: hospitability};
+  var o = {board: board, hospitability: HOSPITABILITY};
   SocketManager.broadcastObject(o);
 }
 
-setInterval(broadcast, 1000);
+var new_branch = function() {
+  var i = Math.floor(Math.random() * FOREST.length);
+  var b = FOREST[i];
+  var j = Math.floor(Math.random() * b.length);
+
+  FOREST.push([b[j]]);
+}
+var grow_branch = function(i: number) {
+  var b = FOREST[i];
+  var leaf = b[b.length-1];
+  var newleaf=[];
+  var d = Math.random();
+
+  if (Math.random() < 0.7){
+    if (Math.random() < 0.5 && leaf[0] != 0){
+      newleaf = [leaf[0]-1, leaf[1]];
+    }
+    else if (leaf[0] != SIZE-1){
+      newleaf = [leaf[0]+1, leaf[1]];
+    }
+  }
+  else if (leaf[1] == 0){
+    return;
+  } else {
+    newleaf = [leaf[0], leaf[1]-1];
+  }
+
+  FOREST[i].push(newleaf);
+}
+
+var grow = function(){
+  if (Math.random() < 0.1 / FOREST.length){
+    new_branch();
+  } else {
+    grow_branch(Math.floor(Math.random() * FOREST.length));
+  }
+}
+
+var evolve = function(){
+  grow();
+  console.log(FOREST.length);
+}
+
+
+setInterval(evolve, 2);
+setInterval(broadcast, 100);
